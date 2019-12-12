1a35354f85f6a2688385a48459aab32daeba3363
Ajay <ajayramesh@bootstrap-hq.com>
2019-12-11 22:57:12 -0600

    init

---
+++ app4arya/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app4arya
+//
+//  Created by Ajay Ramesh on 12/11/19.
+//  Copyright © 2019 Ajay Ramesh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Arya!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app4aryaTests/app4aryaTests.swift
@@ -0,0 +1,34 @@
+//
+//  app4aryaTests.swift
+//  app4aryaTests
+//
+//  Created by Ajay Ramesh on 12/11/19.
+//  Copyright © 2019 Ajay Ramesh. All rights reserved.
+//
+
+import XCTest
+@testable import app4arya
+
+class app4aryaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
